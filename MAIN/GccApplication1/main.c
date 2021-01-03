75b4891345889155e8e305411bfd268546924db8
Peter Zajac <peter.zaja@gmail.com>
2021-01-03 13:05:59 +0100

    Add files via upload

--- MAIN/GccApplication1/main.c
+++ MAIN/GccApplication1/main.c
@@ -12,9 +12,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "i2cmaster.h"
-#include "ds1621.h"
 #include "lcd.h"
-//#include "std.h"
 #include "usart.h"
 #include "a2d.h"
 
@@ -30,36 +28,33 @@ int main(void)
 	double temperature,true_humidity, max_temp,min_temp,max_hum,min_hum,t_s,h_s;
 	long unsigned timer_c;
 	int state=0;
-	//MMA8451_init();
-	i2c_init();
-	LCD_init();
-	//ds1621_init();
-	//uart_init();
-	//io_redirect();
-	register_setup();
-	DDRD = 0x0F; //I/O board:PD4..7 as outputs, for LEDs
-	//DDRC = 0xF0; //I/O board PC0..3 as inputs, for buttons
-	//PORTC = 0x3F; // Enable internal pull at PC0..3 inputs
-	PORTD = 0xF0; // Set output LEDs to off
 
+	i2c_init();
+ 	MMA8451_init();
+	LCD_init();
+	register_setup();
+	DDRD = 0x0F; //I/O board:PD4..7 as inputs, for buttons. 0-3 output
+	DDRC = 0x3B; //I/O 2 input for temp, 6 input for hum
+	PORTC = 0x74; //try this if fail 0x30 // Enable internal pull at 4-6 and 2 inputs
+	PORTD = 0xF0; // internal pull up at 4-7
 	gr_screen(0);     //0 hello, 1 goodbye
 
 	_delay_ms(200);
 
-	temperature=get_temp_lm(adc_read(7));
+	temperature=get_temp_lm(adc_read(2)); 	//setting up starting temperature
 	max_temp = temperature;
 	min_temp = temperature;
 	t_s = temperature;
-	
-	true_humidity=get_humidity(adc_read(6))/(1.0546-0.00216*temperature);
+
+	true_humidity=get_humidity(adc_read(6))/(1.0546-0.00216*temperature);			//setting up starting humidity
 	max_hum=true_humidity;
 	min_hum=true_humidity;
 	h_s=true_humidity;
-	
+
 	while(1)
 	{
 													//MAIN MENU
-		timer_c=millis();
+		timer_c=micros();
 		LCD_set_cursor(0,0);
 		printf("MAIN MENU");
 		LCD_set_cursor(0,1);
@@ -69,17 +64,17 @@ int main(void)
 		LCD_set_cursor(0,3);
 		printf("Heart rate");
 
-		if (!((PINC&B1)==B1)){
+		if (!((PIND&B1)==B1)){
 													//TEMP MENU
 			LCD_clear();
-			while((PINC&B4)==B4)
+			while((PIND&B4)==B4)
 			{
 				if (state==1)
 				{
 					LCD_clear();
 					state=0;
 				}
-				if(state==2)		// THINK OF SOMETHING BETTER
+				if(state==2)
 				{
 					LCD_clear();
 					state=3;
@@ -88,35 +83,35 @@ int main(void)
 				printf("Temperature");
 				LCD_set_cursor(16,3);
 				printf("Back");
-				//LCD_set_cursor(0,2);
 
-
-				if(millis()-timer_c>5000)//takes measurement every 500ms
+				if(micros()-timer_c>5000)//takes measurement every 500ms
 				{
-					timer_c=millis();
-					temperature = get_temp_lm(adc_read(7));
+					timer_c=micros();
+					temperature = get_temp_lm(adc_read(2));
 					if (temperature<min_temp) min_temp=temperature;
 					if (temperature>max_temp) max_temp=temperature;
 
 				}
-				if(abs(t_s-temperature)>=1 && state!=3)
+				if(abs(t_s-temperature)>=0.5 && state!=3)
 					{
 						state=1;LCD_clear();
-						while(abs(t_s-temperature)>=1)
+						while(abs(t_s-temperature)>=0.5)
 						{
+							LCD_set_cursor(0,0);
+							printf("temp=:%02.1f\337C",temperature);
 							LCD_set_cursor(0,1);
 							printf("!ALERT!");
 							LCD_set_cursor(0,2);
-							printf("Temp dif high!");
-							if(millis()-timer_c>5000)//takes measurement every 500ms
+							printf("Temp diff. high!");
+							if(micros()-timer_c>5000)//takes measurement every 500ms
 							{
-								timer_c=millis();
-								temperature = get_temp_lm(adc_read(7));
-								while(millis()-timer_c<5000)
+								timer_c=micros();
+								temperature = get_temp_lm(adc_read(2));
+								while(micros()-timer_c<5000)
 								{
-									PIND|=(1<<3);
+									PORTD|=(1<<3);
 								}
-								PIND&=~(1<<3);
+								PORTD&=~(1<<3);
 							}
 							LCD_set_cursor(0,3);
 							printf("B1 to ignore OR wait!");
@@ -130,18 +125,14 @@ int main(void)
 					}
 
 				LCD_set_cursor(0,1);
-				printf("Current: %02.2f\337C",temperature);
+				printf("Current: %02.1f\337C",temperature);
 				LCD_set_cursor(0,2);
 				printf("Max: %.1f\337C",max_temp);
 				LCD_set_cursor(0,3);
 				printf("Min: %.1f\337C",min_temp);
-				//printf("%.2f",get_temp_lm(adc_read(0)));
-
-
 				}
 
-
-			LCD_clear();//make it into a function
+			LCD_clear();
 			state = 0;
 			_delay_ms(100);
 
@@ -149,29 +140,70 @@ int main(void)
 
 
 
-		if (!((PINC&B2)==B2))
+		if (!((PIND&B2)==B2))
 		{
 																//HUMIDITY MENU
 			_delay_ms(200);
 			LCD_clear();
-			while((PINC&B4)==B4)
+			while((PIND&B4)==B4)
 			{
+				if (state==1)
+				{
+					LCD_clear();
+					state=0;
+				}
+				if(state==2)
+				{
+					LCD_clear();
+					state=3;
+				}
 				LCD_set_cursor(0,0);
 				printf("Humidity");
 				LCD_set_cursor(16,3);
 				printf("Back");
-				if(millis()-timer_c>5000)//takes measurement every 500ms
+				if(micros()-timer_c>5000)//takes measurement every 500ms
 				{
-					timer_c=millis();
+					timer_c=micros();
 					if (max_hum>100)
 					{
 						continue;
 					}
 					else
 					{
-						true_humidity =get_humidity(adc_read(6))/(1.0546-0.00216*temperature);					// add True RH
+						true_humidity =get_humidity(adc_read(6))/(1.0546-0.00216*temperature);
 						if(max_hum<true_humidity) max_hum=true_humidity;
 						if(min_hum>true_humidity) min_hum=true_humidity;
+
+						if((true_humidity<25 || true_humidity>65) && state!=3)
+							{
+								state=1;LCD_clear();
+								while((true_humidity<25 || true_humidity>65))
+								{
+									LCD_set_cursor(0,0);
+									printf("humidity=:%02.1f %",true_humidity);
+									LCD_set_cursor(0,1);
+									printf("!ALERT!");
+									LCD_set_cursor(0,2);
+									printf("HUMIDITY");
+									if(micros()-timer_c>5000)//takes measurement every 500ms
+									{
+										timer_c=micros();
+										true_humidity = get_humidity(adc_read(6))/(1.0546-0.00216*temperature);
+										while(micros()-timer_c<5000)
+										{
+											PORTD|=(1<<3);
+										}
+										PORTD&=~(1<<3);
+									}
+									LCD_set_cursor(0,3);
+									printf("B1 to ignore OR wait!");
+									if(!((PIND&B1)==B1))
+									{
+										state=2;
+										LCD_clear();
+										break;
+									}
+								}
 					}
 				}
 				if (max_hum>100)
@@ -196,22 +228,23 @@ int main(void)
 
 			}
 			LCD_clear();
+			state = 0;
 			_delay_ms(100);
 		}
 
-		if(!((PINC&B3)==B3))
+		if(!((PIND&B3)==B3))
 		{									//HEART RATE MENU
 			_delay_ms(200);
 			LCD_clear();
-			while((PINC&B4)==B4)
+			while((PIND&B4)==B4)
 			{
 				LCD_set_cursor(0,0);
 				printf("Heart Rate");
 				LCD_set_cursor(16,3);
 				printf("Back");
-				/*if(millis()-t>5000)//takes measurement every 500ms
+				/*if(micros()-t>5000)//takes measurement every 500ms
 				{
-					timer_c=millis();
+					timer_c=micros();
 
 
 				}*/
@@ -220,14 +253,6 @@ int main(void)
 			}
 			LCD_clear();
 			_delay_ms(50);
-		}
-
-		if(PINC==(B4&B3&B2))
-		{
-			LCD_clear();
-			_delay_ms(50);
-			gr_screen(1);
-			break;
 		}
 
 	}
